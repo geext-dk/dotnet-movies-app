86cfb924ac11534ee4493fe4093df2dd9f9a88fe
Denis Karpovskiy <geext29@gmail.com>
2019-09-09 22:48:44 +0300

    fix blinking

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,48 @@ const path = require('path');
 
 module.exports = {
   entry: {
-		main: './Assets/main.js'
-	},
+    main: './Assets/main.js'
+  },
   output: {
-    filename: './bundle.js',
-		path: path.resolve(__dirname, 'wwwroot'),
-		publicPath: '/'
-	},
-	mode: "development",
+  filename: './bundle.js',
+    path: path.resolve(__dirname, 'wwwroot'),
+    publicPath: '/'
+  },
+  mode: "development",
   module: {
     rules: [
       {
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							plugins: function() {
-								return [
-									require('precss'),
-									require('autoprefixer')
-								];
-							}
-						}
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
-			}
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
     ]
   }
 };
